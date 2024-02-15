4cbbff937bde97e104c8cbcf77a37814229147f8
enes482 <enesakcm9@gmail.com>
2024-02-15 09:15:21 +0300

    View ile olan Proje dosyası ekle.

---
+++ kitapModel/Controllers/KategoriController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace kitapModel.Controllers
+{
+    public class KategoriController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
