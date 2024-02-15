ed702dafb17b930d3e6c691dfc8ad74d17b121fb
enes482 <enesakcm9@gmail.com>
2024-02-15 09:15:21 +0300

    Proje dosyası ekle.

---
+++ kitapModel/Models/KategoriModeli.cs
@@ -0,0 +1,9 @@
+﻿namespace kitapModel.Models
+{
+    public class KategoriModeli
+    {
+        public int KategoriId { get; set; }
+        public string KategoriAdi { get; set; }
+       List<KitapEvi> kitaplistesi = new List<KitapEvi>();
+    }
+}
---
+++ kitapModel/Models/KitapEvi.cs
@@ -0,0 +1,9 @@
+﻿namespace kitapModel.Models
+{
+    public class KitapEvi
+    {
+        public int KitapId { get; set; }
+        public string KitapAdi { get; set; }
+        public int SayfaSayisi { get; set; }
+    }
+}
